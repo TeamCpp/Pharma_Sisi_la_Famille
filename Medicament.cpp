43203759a170291060ac051377f0ed57feb8ff10
Pierre <pierre.bonneau1@gmail.com>
2015-04-10 11:41:37 +0200

    màj Medicament

--- Medicament.cpp
+++ Medicament.cpp
@@ -2,6 +2,16 @@
 Medicament::Medicament(string nom, vector <string> effets)
 {
 	nom_effets=make_pair(nom,effets);
-	std::cout<<nom_effets.first<<std::endl;
 }
+
+string Medicament::nom() const
+{
+	return nom_effets.first;
+}
+
+vector<string> Medicament::effets() const
+	{
+		return nom_effets.second;
+	};
+
 
